95dcd8444ed4fe75bac3aaa1c49ab31d8f5f5d10
Brian Hechinger <wonko@4amlunch.net>
2017-11-08 14:05:25 -0500

    Adding the framework for the admin commands.
    Adding the ability to list current roles.
    
    And a whole ton of other stuff.

--- command/command.go
+++ command/command.go
@@ -1,13 +1,21 @@
 package command
 
 import (
+	"fmt"
 	proto "github.com/abaeve/chremoas/proto"
+	uauthsvc "github.com/abaeve/auth-srv/proto"
 	"golang.org/x/net/context"
+	"strings"
 )
+
+type ClientFactory interface {
+	NewClient() uauthsvc.UserAuthenticationAdminClient
+}
 
 type Command struct {
 	//Store anything you need the Help or Exec functions to have access to here
-	name    string
+	name string
+	factory ClientFactory
 }
 
 func (c *Command) Help(ctx context.Context, req *proto.HelpRequest, rsp *proto.HelpResponse) error {
@@ -17,11 +25,55 @@ func (c *Command) Help(ctx context.Context, req *proto.HelpRequest, rsp *proto.H
 }
 
 func (c *Command) Exec(ctx context.Context, req *proto.ExecRequest, rsp *proto.ExecResponse) error {
-	rsp.Result = []byte("This will be useful output at some point in the future")
+	var response string
+
+	commandList := map[string]func([]string) string{
+		"help":        help,
+		"add_role":    addRole,
+		"delete_role": notDefined,
+	}
+
+	f, ok := commandList[req.Args[1]]
+	if ok {
+		response = f(req.Args)
+	} else {
+		response = fmt.Sprintf("Not a valid subcommand: %s", req.Args[1])
+	}
+
+	rsp.Result = []byte(response)
 	return nil
 }
 
-func NewCommand(name string) *Command {
-	newCommand := Command{name: name}
+func help(args []string) string {
+	return "This will be help info at some point.\nCan I do line breaks?"
+}
+
+func addRole(args []string) string {
+	role := args[2]
+	roleName := strings.Join(args[3:], " ")
+	return fmt.Sprintf("adding: %s -- %s", role, roleName)
+}
+
+func notDefined(args []string) string {
+	return "This command hasn't been defined yet"
+}
+
+func NewCommand(name string, factory ClientFactory) *Command {
+	newCommand := Command{name: name, factory: factory}
 	return &newCommand
 }
+
+//type UserAuthenticationAdminClient interface {
+//	CharacterRoleAdd(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CharacterRoleRemove(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CorporationAllianceRoleAdd(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CorporationAllianceRoleRemove(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CorporationRoleAdd(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CorporationRoleRemove(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	AllianceRoleAdd(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	AllianceRoleRemove(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	AllianceCharacterLeadershipRoleAdd(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	AllianceCharacterLeadershipRoleRemove(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CorporationCharacterLeadershipRoleAdd(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//	CorporationCharacterLeadershipRoleRemove(ctx context.Context, in *AuthAdminRequest, opts ...client.CallOption) (*AuthAdminResponse, error)
+//}
--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/abaeve/auth-admin-bot/command"
+	uauthsvc "github.com/abaeve/auth-srv/proto"
+	proto "github.com/abaeve/chremoas/proto"
 	"github.com/abaeve/services-common/config"
 	"github.com/micro/go-micro"
-
-	proto "github.com/abaeve/chremoas/proto"
-	"github.com/abaeve/auth-admin-bot/command"
+	"github.com/micro/go-micro/client"
 )
 
 var Version string = "1.0.0"
 var service micro.Service
 
-func main ( ) {
+func main() {
 	service = config.NewService(Version, "template", initialize)
 
 	if err := service.Run(); err != nil {
@@ -22,9 +23,23 @@ func main ( ) {
 
 // This function is a callback from the config.NewService function.  Read those docs
 func initialize(config *config.Configuration) error {
+	authSvcName := config.Bot.AuthSrvNamespace + "." + config.ServiceNames.AuthSrv
+	clientFactory := clientFactory{name: authSvcName, client: service.Client()}
+
 	proto.RegisterCommandHandler(service.Server(),
-		command.NewCommand(config.Name),
+		command.NewCommand(config.Name,
+			&clientFactory,
+		),
 	)
 
 	return nil
+}
+
+type clientFactory struct {
+	name   string
+	client client.Client
+}
+
+func (c clientFactory) NewClient() uauthsvc.UserAuthenticationAdminClient {
+	return uauthsvc.NewUserAuthenticationAdminClient(c.name, c.client)
 }
