d6ec90bf21e483558487b0ef815cf46d2ed38a70
Brian Hechinger <wonko@4amlunch.net>
2018-03-27 14:50:26 -0400

    Cleanup the protobuf messages as we can reuse a message.

--- command/command.go
+++ command/command.go
@@ -49,6 +49,7 @@ var commandList = map[string]command{
 	"member_list":   {listMembers, "List all Filter Members", []string{}},
 	"member_add":    {addMember, "Add Filter Member", []string{}},
 	"member_remove": {removeMember, "Remove Filter Member", []string{}},
+	"member_sync":   {syncMembers, "Sync Filter Membership", []string{}},
 }
 
 var clientFactory ClientFactory
@@ -527,158 +528,56 @@ func syncRoles(ctx context.Context, req *proto.ExecRequest) string {
 		return sendFatal(err.Error())
 	}
 
-	if len(response.AddedRoles) == 0 {
+	if len(response.Added) == 0 {
 		buffer.WriteString("No roles to add")
 	} else {
 		buffer.WriteString("Adding:\n")
-		for r := range response.AddedRoles {
-			buffer.WriteString(fmt.Sprintf("\t%s\n", response.AddedRoles[r]))
+		for r := range response.Added {
+			buffer.WriteString(fmt.Sprintf("\t%s\n", response.Added[r]))
 		}
 	}
 
-	if len(response.RemovedRoles) == 0 {
+	if len(response.Removed) == 0 {
 		buffer.WriteString("\nNo roles to remove")
 	} else {
 		buffer.WriteString("\nRemoving:\n")
-		for r := range response.RemovedRoles {
-			buffer.WriteString(fmt.Sprintf("\t%s\n", response.RemovedRoles[r]))
+		for r := range response.Removed{
+			buffer.WriteString(fmt.Sprintf("\t%s\n", response.Removed[r]))
 		}
 	}
 
 	return fmt.Sprintf("```%s\n```", buffer.String())
 }
 
-//	var fromDiscord []string
-//	var fromChremoas []string
-//
-//	rolesClient := clientFactory.NewRoleClient()
-//	syncedRoles, err := rolesClient.SyncRoles(ctx, &rolesrv.SyncRolesRequest{})
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//		return fmt.Sprintf("```%s```", buffer.String())
-//	}
-//
-//	if len(syncedRoles.Roles) == 0 {
-//		return "```No roles to sync```"
-//	}
-//
-//	for role := range syncedRoles.Roles {
-//		if syncedRoles.Roles[role].Source == "Discord" {
-//			fromDiscord = append(fromDiscord, syncedRoles.Roles[role].Name)
-//		} else if syncedRoles.Roles[role].Source == "Chremoas" {
-//			fromChremoas = append(fromChremoas, syncedRoles.Roles[role].Name)
-//		} else {
-//			// WTF
-//			return "```WTF. Seriously.```"
-//		}
-//	}
-//
-//	if len(fromDiscord) != 0 {
-//		buffer.WriteString("From Discord:\n")
-//		for fd := range fromDiscord {
-//			buffer.WriteString(fmt.Sprintf("\t%s\n", fromDiscord[fd]))
-//		}
-//	}
-//
-//	if len(fromChremoas) != 0 {
-//		buffer.WriteString("From Chremoas:\n")
-//		for fd := range fromChremoas {
-//			buffer.WriteString(fmt.Sprintf("\t%s\n", fromChremoas[fd]))
-//		}
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
-//
-//func addRole(ctx context.Context, req *proto.ExecRequest) string {
-//	var buffer bytes.Buffer
-//
-//	rolesClient := clientFactory.NewRoleClient()
-//	roleName := req.Args[2]
-//	chatServiceGroup := strings.Join(req.Args[3:], " ")
-//
-//	if len(chatServiceGroup) > 0 && chatServiceGroup[0] == '"' {
-//		chatServiceGroup = chatServiceGroup[1:]
-//	}
-//	if len(chatServiceGroup) > 0 && chatServiceGroup[len(chatServiceGroup)-1] == '"' {
-//		chatServiceGroup = chatServiceGroup[:len(chatServiceGroup)-1]
-//	}
-//
-//	_, err := rolesClient.AddRole(ctx, &rolesrv.AddRoleRequest{
-//		Role: &rolesrv.DiscordRole{
-//			Name:     roleName,
-//			RoleNick: chatServiceGroup,
-//		},
-//	})
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//	} else {
-//		buffer.WriteString(fmt.Sprintf("Adding role '%s'\n", chatServiceGroup))
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
-//
-//func deleteRole(ctx context.Context, req *proto.ExecRequest) string {
-//	var buffer bytes.Buffer
-//	rolesClient := clientFactory.NewRoleClient()
-//	roleName := req.Args[2]
-//
-//	_, err := rolesClient.RemoveRole(ctx, &rolesrv.RemoveRoleRequest{
-//		Name: roleName,
-//	})
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//	} else {
-//		buffer.WriteString(fmt.Sprintf("Deleting role: %s\n", roleName))
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
-//
-//func listRoles(ctx context.Context, req *proto.ExecRequest) string {
-//	var buffer bytes.Buffer
-//	rolesClient := clientFactory.NewRoleClient()
-//
-//	output, err := rolesClient.GetRoles(ctx, &rolesrv.GetRolesRequest{})
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//	} else {
-//		if output.String() == "" {
-//			buffer.WriteString("There are no roles defined")
-//		} else {
-//			for role := range output.Roles {
-//				buffer.WriteString(fmt.Sprintf("%s: %s\n",
-//					output.Roles[role].RoleNick,
-//					output.Roles[role].Name,
-//				))
-//			}
-//		}
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
+func syncMembers(ctx context.Context, req *proto.ExecRequest) string {
+	var buffer bytes.Buffer
+	roleClient := clientFactory.NewRoleClient()
+	response, err := roleClient.SyncMembers(ctx, &rolesrv.NilMessage{})
 
-//func myID(ctx context.Context, req *proto.ExecRequest) string {
-//	senderID := strings.Split(req.Sender, ":")[1]
-//
-//	authClient := clientFactory.NewClient()
-//	response, err := authClient.GetRoles(ctx, &uauthsvc.GetRolesRequest{UserId: senderID})
-//	if err != nil {
-//		return err.Error()
-//	}
-//
-//	fmt.Printf("%+v\n", response.Roles)
-//
-//	if len(response.GetRoles()) == 0 {
-//		return fmt.Sprintf("<@%s> You have no roles", senderID)
-//	}
-//	return strings.Join(response.GetRoles(), " ")
-//}
+	if err != nil {
+		return sendFatal(err.Error())
+	}
+
+	if len(response.Added) == 0 {
+		buffer.WriteString("No roles to add")
+	} else {
+		buffer.WriteString("Adding:\n")
+		for r := range response.Added{
+			buffer.WriteString(fmt.Sprintf("\t%s\n", response.Added[r]))
+		}
+	}
+
+	if len(response.Removed) == 0 {
+		buffer.WriteString("\nNo roles to remove")
+	} else {
+		buffer.WriteString("\nRemoving:\n")
+		for r := range response.Removed{
+			buffer.WriteString(fmt.Sprintf("\t%s\n", response.Removed[r]))
+		}
+	}
+
+	return fmt.Sprintf("```%s\n```", buffer.String())
+}
 
 func notDefined(ctx context.Context, req *proto.ExecRequest) string {
 	return "This command hasn't been defined yet"
@@ -689,73 +588,6 @@ func NewCommand(name string, factory ClientFactory) *Command {
 	newCommand := Command{name: name, factory: factory}
 	return &newCommand
 }
-
-//func addDRole(ctx context.Context, req *proto.ExecRequest) string {
-//	var buffer bytes.Buffer
-//	name := strings.Join(req.Args[2:], " ")
-//
-//	client := clientFactory.NewDiscordGatewayClient()
-//	output, err := client.CreateRole(ctx, &discord.CreateRoleRequest{Name: name})
-//	//string GuildId = 1;
-//	//string Name = 2;
-//	//int32 Color = 3;
-//	//bool Hoist = 4;
-//	//int32 Permissions = 5;
-//	//bool Mentionable = 6;
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//	} else {
-//		buffer.WriteString(output.String())
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
-//
-//func listDRoles(ctx context.Context, req *proto.ExecRequest) string {
-//	var buffer bytes.Buffer
-//
-//	client := clientFactory.NewDiscordGatewayClient()
-//	output, err := client.GetAllRoles(ctx, &discord.GuildObjectRequest{})
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//	} else {
-//
-//		w := tabwriter.NewWriter(&buffer, 0, 0, 1, ' ', tabwriter.Debug)
-//		fmt.Fprintln(w, "Position\tName")
-//		for _, v := range output.Roles {
-//			foo := fmt.Sprintf("%d\t%s", v.Position, v.Name)
-//			fmt.Fprintln(w, foo)
-//		}
-//		w.Flush()
-//
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
-//
-//func deleteDRole(ctx context.Context, req *proto.ExecRequest) string {
-//	var buffer bytes.Buffer
-//	name := strings.Join(req.Args[2:], " ")
-//
-//	client := clientFactory.NewDiscordGatewayClient()
-//	output, err := client.DeleteRole(ctx, &discord.DeleteRoleRequest{Name: name})
-//	//string GuildId = 1;
-//	//string Name = 2;
-//	//int32 Color = 3;
-//	//bool Hoist = 4;
-//	//int32 Permissions = 5;
-//	//bool Mentionable = 6;
-//
-//	if err != nil {
-//		buffer.WriteString(err.Error())
-//	} else {
-//		buffer.WriteString(output.String())
-//	}
-//
-//	return fmt.Sprintf("```%s```", buffer.String())
-//}
 
 func sendSuccess(message string) string {
 	return fmt.Sprintf(":white_check_mark: %s", message)
